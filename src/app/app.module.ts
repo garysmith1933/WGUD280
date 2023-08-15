af06dcc1e35a3d8fde0c9bc5404d75ad7d424354
Gary Smith <garysmith193358@gmail.com>
2023-08-15 18:44:32 -0400

    initalized angular template, added world map svg

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { WorldComponent } from './world/world.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WorldComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
